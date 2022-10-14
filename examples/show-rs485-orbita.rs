627f6f8742f7282a3fae96c05d5ea5f307c526fc
Pierre Rouanet <pierre.rouanet@gmail.com>
2022-10-14 14:30:17 +0200

    Update show-rs485-orbita.rs

--- examples/show-rs485-orbita.rs
+++ examples/show-rs485-orbita.rs
@@ -3,7 +3,7 @@ use std::{error::Error, thread, time::Duration};
 use rustypot::DynamixelSerialIO;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut io = DynamixelSerialIO::new("/dev/ttyACM0", 500_000, Duration::from_millis(100));
+    let mut io = DynamixelSerialIO::new("/dev/ttyACM0", 500_000, Duration::from_millis(100))?;
 
     println!("{:?}", io.ping(40)?);
 
