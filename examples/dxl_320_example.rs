4a401739a6ba8ed3652049f05ee55048c21da5ad
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-01-03 15:04:44 +0100

    Add xl320 example.

---
+++ examples/dxl_320_example.rs
@@ -0,0 +1,39 @@
+use std::{error::Error, thread, time::Duration};
+
+use rustypot::device::xl320;
+use rustypot::DynamixelSerialIO;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    let mut serial_port = serialport::new("/dev/ttyACM0", 1_000_000)
+        .timeout(Duration::from_millis(10))
+        .open()?;
+
+    let io = DynamixelSerialIO::v2();
+
+    loop {
+        // println!("PING {:?}", io.ping(serial_port.as_mut(), 30));
+
+        // let temp = xl320::read_present_temperature(&io, serial_port.as_mut(), 30)?;
+        // println!("{:?}", temp);
+
+        // let pos = xl320::read_present_position(&io, serial_port.as_mut(), 30)?;
+        // println!("{:?}", pos);
+
+        // let pos = xl320::read_present_position(&io, serial_port.as_mut(), 31)?;
+        // println!("{:?}", pos);
+
+        // let pos = xl320::sync_read_present_position(&io, serial_port.as_mut(), &[30, 31])?;
+        // println!("{:?}", pos);
+
+        // let temp = xl320::sync_read_present_temperature(&io, serial_port.as_mut(), &[30, 31])?;
+        // println!("{:?}", temp);
+
+        xl320::sync_write_goal_position(&io, serial_port.as_mut(), &[30, 31], &[0, 512])?;
+        thread::sleep(Duration::from_millis(500));
+
+        xl320::sync_write_goal_position(&io, serial_port.as_mut(), &[31, 30], &[0, 512])?;
+        thread::sleep(Duration::from_millis(500));
+    }
+}
