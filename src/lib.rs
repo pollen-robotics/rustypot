19f30b2c99bb7b9539a5cbfd5217d0ac0ba21eb3
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-01-12 16:56:40 +0100

    Remove flush after error.

--- src/lib.rs
+++ src/lib.rs
@@ -35,10 +35,6 @@ impl DynamixelSerialIO {
             Protocols::V1(p) => p.ping(serial_port, id),
             Protocols::V2(p) => p.ping(serial_port, id),
         }
-        .map_err(|e| {
-            let _ = self.flush(serial_port);
-            e
-        })
     }
 
     pub fn read(
@@ -52,10 +48,6 @@ impl DynamixelSerialIO {
             Protocols::V1(p) => p.read(serial_port, id, addr, length),
             Protocols::V2(p) => p.read(serial_port, id, addr, length),
         }
-        .map_err(|e| {
-            let _ = self.flush(serial_port);
-            e
-        })
     }
 
     pub fn write(
@@ -69,10 +61,6 @@ impl DynamixelSerialIO {
             Protocols::V1(p) => p.write(serial_port, id, addr, data),
             Protocols::V2(p) => p.write(serial_port, id, addr, data),
         }
-        .map_err(|e| {
-            let _ = self.flush(serial_port);
-            e
-        })
     }
 
     pub fn sync_read(
@@ -86,10 +74,6 @@ impl DynamixelSerialIO {
             Protocols::V1(p) => p.sync_read(serial_port, ids, addr, length),
             Protocols::V2(p) => p.sync_read(serial_port, ids, addr, length),
         }
-        .map_err(|e| {
-            let _ = self.flush(serial_port);
-            e
-        })
     }
 
     pub fn sync_write(
@@ -103,19 +87,12 @@ impl DynamixelSerialIO {
             Protocols::V1(p) => p.sync_write(serial_port, ids, addr, data),
             Protocols::V2(p) => p.sync_write(serial_port, ids, addr, data),
         }
-        .map_err(|e| {
-            let _ = self.flush(serial_port);
-            e
-        })
     }
 
     pub fn flush(&self, serial_port: &mut dyn serialport::SerialPort) -> Result<()> {
-        log::debug!("Flushing serial port...");
-
-        let n = serial_port.bytes_to_read()? as usize;
-        let mut buff = vec![0u8; n];
-        serial_port.read_exact(&mut buff)?;
-
-        Ok(())
+        match &self.protocol {
+            Protocols::V1(v) => v.flush(serial_port),
+            Protocols::V2(v) => v.flush(serial_port),
+        }
     }
 }
