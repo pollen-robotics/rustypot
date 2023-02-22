6268f60ce9c4c718ee56e9d375d1a10a7c518ce2
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-02-22 10:33:26 +0100

    Fix Pos/Speed/Load getter PID and temperature.

--- src/device/orbita_foc.rs
+++ src/device/orbita_foc.rs
@@ -1,17 +1,23 @@
 use crate::device::*;
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct DiskValue<T> {
     pub top: T,
     pub middle: T,
     pub bottom: T,
 }
 
-#[derive(Debug)]
-pub struct PositionSpeedLoad {
-    pub position: f32,
-    pub speed: f32,
-    pub load: f32,
+#[derive(Clone, Copy, Debug)]
+pub struct DiskPositionSpeedLoad {
+    pub position: DiskValue<f32>,
+    pub speed: DiskValue<f32>,
+    pub load: DiskValue<f32>,
+}
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Pid {
+    pub p: f32,
+    pub i: f32,
+    pub d: f32,
 }
 
 const MISSING: u8 = 255;
@@ -33,7 +39,7 @@ reg_read_write!(velocity_d_gain, MISSING, f32);
 reg_read_write!(velocity_out_ramp, MISSING, f32);
 reg_read_write!(velocity_low_pass_filter, MISSING, f32);
 
-reg_read_write!(angle_pid, MISSING, DiskValue::<f32>);
+reg_read_write!(angle_pid, MISSING, Pid);
 reg_read_write!(angle_p_gain, MISSING, f32);
 reg_read_write!(angle_i_gain, MISSING, f32);
 reg_read_write!(angle_d_gain, MISSING, f32);
@@ -48,11 +54,7 @@ reg_read_write!(top_goal_position, 50, f32);
 reg_read_write!(middle_goal_position, 54, f32);
 reg_read_write!(bottom_goal_position, 58, f32);
 
-reg_read_only!(
-    present_position_speed_load,
-    90,
-    DiskValue::<PositionSpeedLoad>
-);
+reg_read_only!(present_position_speed_load, 90, DiskPositionSpeedLoad);
 reg_read_only!(present_position, 90, DiskValue::<f32>);
 reg_read_only!(top_present_position, 90, f32);
 reg_read_only!(middle_present_position, 94, f32);
@@ -66,7 +68,16 @@ reg_read_only!(top_present_load, 114, f32);
 reg_read_only!(middle_present_load, 118, f32);
 reg_read_only!(bottom_present_load, 122, f32);
 
-reg_read_only!(present_temperature, 130, f32);
+reg_read_only!(present_temperature, 130, DiskValue::<f32>);
+reg_read_only!(top_present_temperature, 130, f32);
+reg_read_only!(middle_present_temperature, 130, f32);
+reg_read_only!(bottom_present_temperature, 130, f32);
+
+impl<T: PartialEq> PartialEq for DiskValue<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.top == other.top && self.middle == other.middle && self.bottom == other.bottom
+    }
+}
 
 impl DiskValue<f32> {
     pub fn from_le_bytes(bytes: [u8; 12]) -> Self {
@@ -87,39 +98,39 @@ impl DiskValue<f32> {
     }
 }
 
-impl PositionSpeedLoad {
-    fn from_le_bytes(bytes: [u8; 12]) -> Self {
-        PositionSpeedLoad {
-            position: f32::from_le_bytes(bytes[0..4].try_into().unwrap()),
-            speed: f32::from_le_bytes(bytes[4..8].try_into().unwrap()),
-            load: f32::from_le_bytes(bytes[8..12].try_into().unwrap()),
-        }
-    }
-    fn to_le_bytes(&self) -> [u8; 12] {
-        let mut bytes = Vec::new();
-
-        bytes.extend_from_slice(&self.position.to_le_bytes());
-        bytes.extend_from_slice(&self.speed.to_le_bytes());
-        bytes.extend_from_slice(&self.load.to_le_bytes());
-
-        bytes.try_into().unwrap()
-    }
-}
-
-impl DiskValue<PositionSpeedLoad> {
-    pub fn from_le_bytes(bytes: [u8; 36]) -> Self {
-        DiskValue {
-            top: PositionSpeedLoad::from_le_bytes(bytes[0..4].try_into().unwrap()),
-            middle: PositionSpeedLoad::from_le_bytes(bytes[4..8].try_into().unwrap()),
-            bottom: PositionSpeedLoad::from_le_bytes(bytes[8..12].try_into().unwrap()),
+impl Pid {
+    pub fn from_le_bytes(bytes: [u8; 12]) -> Self {
+        Pid {
+            p: f32::from_le_bytes(bytes[0..4].try_into().unwrap()),
+            i: f32::from_le_bytes(bytes[4..8].try_into().unwrap()),
+            d: f32::from_le_bytes(bytes[8..12].try_into().unwrap()),
         }
     }
     pub fn to_le_bytes(&self) -> [u8; 12] {
         let mut bytes = Vec::new();
 
-        bytes.extend_from_slice(&self.top.to_le_bytes());
-        bytes.extend_from_slice(&self.middle.to_le_bytes());
-        bytes.extend_from_slice(&self.bottom.to_le_bytes());
+        bytes.extend_from_slice(&self.p.to_le_bytes());
+        bytes.extend_from_slice(&self.i.to_le_bytes());
+        bytes.extend_from_slice(&self.d.to_le_bytes());
+
+        bytes.try_into().unwrap()
+    }
+}
+
+impl DiskPositionSpeedLoad {
+    pub fn from_le_bytes(bytes: [u8; 36]) -> Self {
+        DiskPositionSpeedLoad {
+            position: DiskValue::from_le_bytes(bytes[0..12].try_into().unwrap()),
+            speed: DiskValue::from_le_bytes(bytes[12..24].try_into().unwrap()),
+            load: DiskValue::from_le_bytes(bytes[24..36].try_into().unwrap()),
+        }
+    }
+    pub fn to_le_bytes(&self) -> [u8; 36] {
+        let mut bytes = Vec::new();
+
+        bytes.extend_from_slice(&self.position.to_le_bytes());
+        bytes.extend_from_slice(&self.speed.to_le_bytes());
+        bytes.extend_from_slice(&self.load.to_le_bytes());
 
         bytes.try_into().unwrap()
     }
