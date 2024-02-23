b92248c9cd813c054497afbd07a2581e8507e916
RemiFabre <remifabre1800@gmail.com>
2024-02-23 15:05:03 +0000

    Saving working version on beta

--- src/bin/dxl_scan.rs
+++ src/bin/dxl_scan.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     println!("Scanning...");
-    let mut serial_port = serialport::new(args.serial_port, 1_000_000)
+    let mut serial_port = serialport::new(args.serial_port, 2_000_000)
         .timeout(Duration::from_millis(10))
         .open()?;
 
