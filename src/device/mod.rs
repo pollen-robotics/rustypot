7bfc680d0eea9e10d54bf92429d24ae055c25e81
gospar <antun.skuric@outlook.com>
2024-05-03 10:25:55 +0200

    added the check for the lenght of receved data before running try_into().unwrap() - #52

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -18,6 +18,7 @@ macro_rules! reg_read_only {
             id: u8,
         ) -> Result<$reg_type> {
             let val = io.read(serial_port, id, $addr, size_of::<$reg_type>().try_into().unwrap())?;
+            check_response_size!(&val, $reg_type);
             let val = $reg_type::from_le_bytes(val.try_into().unwrap());
 
             Ok(val)
@@ -30,6 +31,7 @@ macro_rules! reg_read_only {
             ids: &[u8],
         ) -> Result<Vec<$reg_type>> {
             let val = io.sync_read(serial_port, ids, $addr, size_of::<$reg_type>().try_into().unwrap())?;
+            check_response_size!(&val, $reg_type);
             let val = val
                 .iter()
                 .map(|v| $reg_type::from_le_bytes(v.as_slice().try_into().unwrap()))
@@ -91,8 +93,9 @@ macro_rules! reg_write_only_fb {
                 val: $reg_type,
             ) -> Result<$fb_type> {
                 let fb=io.write_fb(serial_port, id, $addr, &val.to_le_bytes())?;
-		let fb = $fb_type::from_le_bytes(fb.try_into().unwrap());
-		Ok(fb)
+                check_response_size!(&fb, $fb_type);
+                let fb = $fb_type::from_le_bytes(fb.try_into().unwrap());
+                Ok(fb)
             }
 
             #[doc = concat!("Sync write register *", stringify!($name), "* (addr: ", stringify!($addr), ", type: ", stringify!($reg_type), ")")]
@@ -130,6 +133,21 @@ macro_rules! reg_read_write_fb {
     ($name:ident, $addr:expr, $reg_type:ty, $fb_type: ty) => {
         reg_read_only!($name, $addr, $reg_type);
         reg_write_only_fb!($name, $addr, $reg_type, $fb_type);
+    };
+}
+
+// Check if the response size is correct
+// If not, return an error
+// response is a Vec<u8>
+macro_rules! check_response_size {
+    ($response:expr, $reg_type:ty) => {
+        {
+            let response = $response;
+            if response.len() != std::mem::size_of::<$reg_type>() {
+                let message = format!("Invalid response size, expected {} received {}", std::mem::size_of::<$reg_type>(), response.len());
+                return Err(message.into());
+            }
+        }
     };
 }
 
